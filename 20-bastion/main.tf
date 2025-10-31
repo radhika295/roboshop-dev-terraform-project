987c9d42102df725ee25bff7c967ef2d7e866c2e
Radhika <sairadhika.iiitn@gmail.com>
2025-10-31 13:31:19 +0530

    ansible-playbook

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -4,6 +4,12 @@ resource "aws_instance" "bastion" {
     vpc_security_group_ids = [local.bastion_sg_id]
     iam_instance_profile = aws_iam_instance_profile.bastion.name
     subnet_id = local.public_subnet_ids
+    root_block_device {
+      volume_size           = 60  # Size in GiB
+      volume_type           = "gp3" # or "gp2", "io1", etc.
+      delete_on_termination = true # Whether to delete the volume when the instance is terminated
+    
+    }
     user_data = file("bastion.sh")
     tags = merge (
         local.common_tags,
