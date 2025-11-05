8d2eaf663977b002801b417509a3ad77f7cf118a
Radhika <sairadhika.iiitn@gmail.com>
2025-11-05 23:35:51 +0530

    ansible-playbook

--- 60-catalouge/main.tf
+++ 60-catalouge/main.tf
@@ -192,7 +192,7 @@ resource "terraform_data" "catalogue_local" {
     aws_instance.catalogue.id
   ]
   
-  depends_on = [aws_autoscaling_policy.catalogue]
+  depends_on = [aws_autoscaling_policy.example]
   provisioner "local-exec" {
     command = "aws ec2 terminate-instances --instance-ids ${aws_instance.catalogue.id}"
   }
