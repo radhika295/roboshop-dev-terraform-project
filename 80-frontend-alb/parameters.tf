086e673f12764d155de98634f7143c010ee7e53a
Radhika <sairadhika.iiitn@gmail.com>
2025-11-16 18:00:38 +0530

    terraform

--- 80-frontend-alb/parameters.tf
+++ 80-frontend-alb/parameters.tf
@@ -1,5 +1,5 @@
-resource "aws_ssm_parameter" "frontend_lb_listener_arn" {
-  name  = "/${var.project_name}/${var.environment}/frontend_lb_listener_arn"
+resource "aws_ssm_parameter" "frontend_alb_listener_arn" {
+  name  = "/${var.project_name}/${var.environment}/frontend_alb_listener_arn"
   type  = "String"
-  value = aws_lb_listener.frontend_lb.arn
+  value = aws_lb_listener.frontend_alb.arn
 }
--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,5 +1,12 @@
-module "components" {
-    source = "../../terraform-roboshop-component"
-    component = var.component
-    rule_priority = var.rule_priority
+# module "components" {
+#     source = "../../terraform-roboshop-component"
+#     component = var.component
+#     rule_priority = var.rule_priority
+#  }
+
+ module "components" {
+    for_each = var.component
+    source = "git::https://github.com/radhika295/terraform-roboshop-component.git"
+    component = each.key
+    rule_priority = each.value.rule_priority
  }
